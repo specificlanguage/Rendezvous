c8905d0c7aca79fac6dfac332c1e6e99f3fb231c
specificlanguage <specificlanguage@gmail.com>
2023-09-21 14:20:07 -0400

    Initial commit
    - Setup react app, tailwind, chakra, prettier, husky, react-router, eslint

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import { Skeleton, Stack } from "@chakra-ui/react";
+
+function App() {
+    return (
+        <Stack>
+            <Skeleton height="50px" width="100px" />
+        </Stack>
+    );
+}
+
+export default App;
