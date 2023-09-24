12f0cd0df3072a56d48cde0ff9df2b8f489d8e24
specificlanguage <specificlanguage@gmail.com>
2023-09-23 23:13:04 -0400

    fix: Redirect to main page if logged in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,14 +2,37 @@ import LoginForm from "../components/Auth/LoginForm.tsx";
 import SignupForm from "../components/Auth/SignupForm.tsx";
 import {
     Card,
+    Spinner,
     Tab,
     TabList,
     TabPanel,
     TabPanels,
     Tabs,
 } from "@chakra-ui/react";
+import { FIREBASE_AUTH } from "../lib/firebase.ts";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import { Navigate } from "react-router-dom";
 
 export default function LoginPage() {
+    const [user, setUser] = useState<User | null | undefined>(undefined);
+
+    useEffect(() => {
+        FIREBASE_AUTH.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+    }, []);
+
+    if (user === undefined) {
+        return (
+            <div className="mx-auto">
+                <Spinner size="xl" />
+            </div>
+        );
+    } else if (user) {
+        return <Navigate to="/" />;
+    }
+
     return (
         <Card p={5} bg={"neutral-200"} _dark={{ bg: "neutral-900" }}>
             <div>
